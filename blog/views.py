9d915b4df389b3f7260f05713c5c0c9db214ecf3
Vinay <krishnan.vinay@gmail.com>
2016-06-09 21:50:32 +0530

    incomplete - sending email.

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 class PostListView(ListView):
@@ -10,6 +11,8 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
+'''
+# Replaced by class based views PostListView
 def post_list(request):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3)
@@ -26,6 +29,7 @@ def post_list(request):
                   'blog/post/list.html',
                   {'page':page,
                    'posts':posts})
+'''
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, 
@@ -38,3 +42,18 @@ def post_detail(request, year, month, day, post):
     return render(request, 
                   'blog/post/detail.html', 
                   {'post': post})
+    
+def post_share(request, post_id):
+    # Retrieves post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+            
+    return render(request, 'blog/post/share.html', {'post':post,
+                                                    'form':form'})
